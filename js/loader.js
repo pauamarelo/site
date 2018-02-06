ff3523153fc0e4b7cadf0e6c18d8d979be371370
brdppl <beco_sampa@hotmail.com>
2018-02-05 23:47:36 -0200

    update

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,6 @@
-$(document).ready(function(){
-   $('#loader').show();
-   var totalImages = $(".materialboxed").length;
-   var iLoaded = 0;
-   $(".materialboxed").each(function () 
-   {
-     $(this).bind("load", function()
-     {
-       iLoaded++;
-       if(iLoaded == totalImages)
-       {
-          $('#loader').hide();
-       }
-       $(this).attr('src', $(this).attr("src"));
-    });
-  });
+jQuery(document).ready(function() {
+	$('#loader').show();
+	$('.materialboxed').load(function(){
+		$('#loader').hide();
+	});
 });
