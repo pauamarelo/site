db3ab2fc4507264b0177dfe1f14bb904956dd5bf
brdppl <beco_sampa@hotmail.com>
2018-02-05 23:32:28 -0200

    update

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,8 @@
-// Este evendo é acionado após o carregamento da página
-jQuery(document).ready(function() {
-    //Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
-    jQuery("#loader").delay(1000).fadeOut("slow");
+// show loading image
+$('#loader').show();
+
+// main image loaded ?
+$('img').on('load', function(){
+  // hide/remove the loading image
+  $('#loader').hide();
 });
