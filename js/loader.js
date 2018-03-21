30a3e7749d51e9f263df6747d0641017de411a01
brdppl <beco_sampa@hotmail.com>
2018-03-20 22:04:00 -0300

    update

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,3 @@
 window.onload = function(){
-<<<<<<< HEAD
-	document.getElementById("preloader").style.display = "none"
+	document.getElementById("preloader").style.display = "none";
 }
-=======
-  document.getElementById("preloader").style.display = "none"
-}
->>>>>>> 440913b5e7254c0e1f407a83b0c450bfb7a4b993
