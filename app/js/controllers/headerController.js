f7c8dd383f54fb542404010f6d9c500b63736647
pauamarelo <clanpauamarelo@hotmail.com>
2019-07-04 22:27:41 -0300

    update scroll function

--- app/js/controllers/headerController.js
+++ app/js/controllers/headerController.js
@@ -6,58 +6,22 @@
     app.controller("headerCtrl", function($scope, $timeout, $state) {
         const vm = this
 
-        vm.goToTop = function() {
-            if ($state.is('inicio')) {
-                $('html, body').animate({
-                    scrollTop: 0
-                }, 300)
-            } else {
-                $state.go('inicio')
-            }
-        }
-    
-        vm.goToLine = function() {
-            if ($state.is('inicio')) {
-                $('html, body').animate({
-                    scrollTop: $("#line").offset().top - 60
-                }, 300)
-            } else {
-                $state.go('inicio')
-                $timeout(function () {
+        vm.goToSection = function(id) {
+            if($state.is('inicio')) {
+                if(!id) {
                     $('html, body').animate({
-                        scrollTop: $("#line").offset().top - 60
+                        scrollTop: 0
                     }, 300)
-                }, 50)
-            }
-        }
-    
-        vm.goToMapa = function() {
-            if ($state.is('inicio')) {
-                $('html, body').animate({
-                    scrollTop: $("#mapa").offset().top - 60
-                }, 300)
+                } else {
+                    $('html, body').animate({
+                        scrollTop: $(id).offset().top - 60
+                    }, 300)
+                }
             } else {
                 $state.go('inicio')
-                $timeout(function () {
-                    $('html, body').animate({
-                        scrollTop: $("#mapa").offset().top - 60
-                    }, 300)
-                }, 50)
-            }
-        }
-    
-        vm.goToPeneira = function() {
-            if ($state.is('inicio')) {
                 $('html, body').animate({
-                    scrollTop: $("#peneira").offset().top - 60
+                    scrollTop: $(id).offset().top - 60
                 }, 300)
-            } else {
-                $state.go('inicio')
-                $timeout(function() {
-                    $('html, body').animate({
-                        scrollTop: $("#peneira").offset().top - 60
-                    }, 300)
-                }, 50)
             }
         }
     })
